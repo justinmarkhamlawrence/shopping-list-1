8a897887d9069348fbd400b1ee58ea28661b575c
justinmarkhamlawrence <justinmarkhamlawrence@gmail.com>
2016-07-19 12:18:12 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	
+});
+//Enter items by clicking "add item" or clicking return
+	//enter text into item field
+		//trigger function
+			//listen for mouseclick and enter key
+				//enter nothing to input if field is empty
+			//call function to add input to span of li
+			//add span to li with all additional pieces(ie. buttons, spans, etc.)
+			//append or prepend li to ul
+//check and uncheck items
+	//use "this" to target specific element
+	//if class not present, add class to span
+	//else remove class
+//rm items using button
+	//rm "this" item from ul
